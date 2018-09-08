6452c97bdd52cb7200534f91e63d52d03f777637
Max Friedman <mr.awesome10000@gmail.com>
2018-09-07 22:29:29 -0700

    style

--- word_manip.py
+++ word_manip.py
@@ -2,17 +2,20 @@ import mod, sys, random, pickle
 #python word_manip.py
 
 def start():
-	words = pickle.load(open("list_of_Eng_words.txt","rb"))
+	with open("list_of_Eng_words.txt", "rb") as file:
+		words = pickle.load(file))
+
 	blacklisted = []
-	dict = [words,blacklisted]
-	pickle.dump(dict, open("list_of_Eng_words.txt","wb"))
-	
+	dict = [words, blacklisted]
+	with open("list_of_Eng_words.txt", "wb") as file:
+		pickle.dump(dict, file)
+
 
 
 def rand_words(num):
 	ans = []
 	for i in range(num):
-		ans.append(mod.words[random.randint(0,len(mod.words)-1)])
+		ans.append(mod.words[random.randint(0, len(mod.words) - 1)])
 	return ans
 
 def length_words():
@@ -37,17 +40,19 @@ def total_letters():
 def start_with(start):
 	ans = []
 	for i in mod.words:
-		if mod.str_start(start,i):
+		if mod.str_start(start, i):
 			ans.append(i)
 	return ans
 
 def del_dupe():
-	words = pickle.load(open("list_of_Eng_words.txt","rb"))
+	with open("list_of_Eng_words.txt", "rb") as file:
+		words = pickle.load()
 	
-	for j,i in enumerate(mod.words):
+	for j, i in enumerate(mod.words):
 		if mod.count(i) > 1:
 			mod.words.pop(j)
 	
-	pickle.dump(words, open("list_of_Eng_words.txt","wb"))
+	with open("list_of_Eng_words.txt", "wb") as file:
+		pickle.dump(words, file)
 
 start()
