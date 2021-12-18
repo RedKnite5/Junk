33bdee3f5375931ab29d383b3bff0723b15135d4
RedKnite5 <mr.awesome10000@gmail.com>
2021-12-18 08:52:41 -0800

    column, row, and box checks

--- stuff.py
+++ stuff.py
@@ -3,9 +3,47 @@
 
 
 
+class MyDict(dict):
+	def __getitem__(self, attr):
+		print(f"Getting {attr!r}")
+		return super().__getitem__(attr)
+	
+	def __setitem__(self, attr, val):
+		print(f"Setting {attr!r} to {val!r}")
+		super().__setitem__(attr, val)
+	
+	def __repr__(self):
+		return f"MyDict({super().__repr__()})"
 
 
+class Meta(type):
+	@classmethod
+	def __prepare__(cls, name, bases, **kwds):
+		print(f"{name = !r}\n{bases = !r}\n{kwds = !r}")
+		
+		return MyDict()
+	
+	def __new__(cls, name, bases, ns):
+		print(f"{ns = }")
+		
+		obj = super().__new__(cls, name, bases, ns)
+		
+		#obj.__dict__["hi"] = "test"
+		#setattr(obj, "__dict__", ns)
+		return obj
 
 
+class Base(metaclass=Meta):
+	def __init__(self):
+		self.x = 5
+	
 
+print(MyDict())
+
+a = Base()
+
+
+print(Base.__dict__)
+
+print(getattr(a, "x"))
 
--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,6 @@
 # sudoku.py
 
-import numpy as np
+#import numpy as np
 
 string = """
 000005409
@@ -16,38 +16,116 @@ string = """
 
 class Board(object):
 	def __init__(self, string: str):
-		digits = {9, 8, 7, 6, 5, 4, 3, 2, 1}
+		
 		data = string.split("\n")
-		self.possiblities = [
-			[{int(data[j][i])} if data[j][i] else digits.copy()
+		
+		self.size = len(data)
+		digits = {i for i in range(1, self.size + 1)}
+		#{9, 8, 7, 6, 5, 4, 3, 2, 1}
+		
+		self.board = [
+			[{int(data[j][i])} if int(data[j][i]) else digits.copy()
 			for i
-			in range(9)] for j in range(9)
+			in range(self.size)] for j in range(self.size)
 		]
 
-	def check_verticle_missing(self, column: int):
+	def check_verticle(self, column: int):
 		present = set()
+		modified = False
 		
 		for row in self.board:
 			square = row[column]
-			if square != {0}:
-				present.add(square)
+			if len(square) == 1:
+				present.add(next(iter(square)))
 		
 		for row in self.board:
 			square = row[column]
 			if len(square) > 1:
+				if not modified and not present.isdisjoint(square):
+					modified = True
 				square -= present
-	
-	
 		
+		return modified
+
+	def check_horizontal(self, row: int):
+		present = set()
+		modified = False
 		
+		for square in self.board[row]:
+			if len(square) == 1:
+				present.add(next(iter(square)))
+		
+		for square in self.board[row]:
+			if len(square) > 1:
+				if not modified and not present.isdisjoint(square):
+					modified = True
+				square -= present
+		
+		return modified
+
+	def check_box(self, box: int):
+		column = box % 3
+		row = box // 3
+		
+		present = set()
+		modified = False
+		
+		squares = [self.board[3 * (box // 3) + i // 3][3 * (box % 3) + i % 3] for i in range(self.size)]
+		
+		for square in squares:
+			if len(square) == 1:
+				present.add(next(iter(square)))
+		
+		for square in squares:
+			if len(square) > 1:
+				if not modified and not present.isdisjoint(square):
+					modified = True
+				square -= present
+		
+		return modified
+	
+	def show(self):
+		s = "-" * 4 * self.size + "\n|"
+		
+		for row in self.board:
+			for i in range(3):
+				for square in row:
+					for j in range(1, 4):
+						d = i * 3 + j
+						if d in square:
+							s += str(d)
+						else:
+							s += " "
+					s += "|"
+				s += "\n|"
+			s += "-" * 4 * self.size + "\n|"
+		
+		return s[:-1]
+		
+	def __str__(self):
+		return self.show()
+
+	def solve(self):
+	
+		while self.basic_pass():
+			pass
+		
+	def basic_pass(self) -> bool:
+		mod = False
+		for i in range(self.size):
+			mod = mod or self.check_verticle(i)
+			mod = mod or self.check_horizontal(i)
+			mod = mod or self.check_box(i)
+			
+		return mod
 		
 
 
 board = Board(string[1:-1])
 
-m = board.check_verticle_missing(0)
-print(m)
-
+print(board)
+board.solve()
+print(board)
 
 	
 	
