942c2e53575338fad8aa5f3c68f93c64b1ff7624
Max Friedman <33992887+RedKnite5@users.noreply.github.com>
2023-03-24 06:53:42 +0000

    fix lower bound

--- euler85.py
+++ euler85.py
@@ -4,28 +4,20 @@ def count_rect(m: int, n: int) -> int:
   area = m * n
   return (area * (n + m + area + 1)) // 4
 
-
-# (area * (n + m + area + 1)) // 4 = x
-# n*n * (2*n + n*n + 1) = 4x
-# 2*n^3 + n^4 + n^2 = 4x
-# n^4 = 4x/4
-# n = x^.25
-
 def find_closest(n: int) -> int:
-  start = int(n ** 0.25) - 1  # lower bound
   end = int((2 * n - 1) ** .5) + 2  # upper bound
   
-  closest = (start*start, count_rect(start, start))
+  closest = (1, 1)
   sq_dist = (n - closest[1]) ** 2
   dist = abs(n - closest[1])
-  for i in range(start, end):
+  for i in range(1, end):
     for j in range(i, end):
       rects = count_rect(i, j)
       if (n - rects) ** 2 < sq_dist:
         closest = (i*j, rects)
         sq_dist = (n - rects) ** 2
         dist = abs(n - rects)
-        print(f"{i=} {j=}")
+        #print(f"{i=} {j=}")
       elif (rects - n) >= n + dist:
         break
   return closest[0]
